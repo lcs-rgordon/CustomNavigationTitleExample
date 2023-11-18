27d684643991f93f11e11bec3a7b4a9f81b8c37a
Russell Gordon <rgordon@lcs.on.ca>
2023-11-18 11:01:44 -0500

    Initial Commit

---
+++ CustomNavigationTitleExample/CustomNavigationTitleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationTitleExampleApp.swift
+//  CustomNavigationTitleExample
+//
+//  Created by Russell Gordon on 2023-11-18.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationTitleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
